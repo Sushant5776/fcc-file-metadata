d1fc9967508a534a299e1fd37d8ad684a0615604
Sushant Garudkar <garudkarsushant7@gmail.com>
2022-04-24 00:22:41 +0530

    Project Complete

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
+const multer = require('multer')
+const upload = multer({ limits: { files: 1 } })
 
-var app = express();
+const app = express()
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
-
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-});
+  res.sendFile(process.cwd() + '/views/index.html')
+})
+
+app.post('/api/fileanalyse', upload.single('upfile'), async (req, res) => {
+  const { originalname, mimetype, size } = req.file
+  res.status(200).json({ name: originalname, type: mimetype, size })
+})
 
 
-
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
-});
+})
